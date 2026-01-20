9cf651d04d965c9d8266f9efefa778a93c9dac58
Mihal Ivanov <60715096+M-II-M@users.noreply.github.com>
2026-01-20 17:30:14 +0300

    - Убрал методы которые больше не используются - Добавит доки к методам

--- src/Services/ModelMetaService.php
+++ src/Services/ModelMetaService.php
@@ -10,12 +10,18 @@ class ModelMetaService
         private ModelRegistryService $registryService
     ) {}
 
+    /**
+     * Получение методанных по модели
+     * @param string $alias алиас модели
+     * @return array
+     * @throws \Exception
+     */
     public function getModelMetadata(string $alias): array
     {
         $resource = $this->registryService->getResourceByAlias($alias);
 
         if (!$resource) {
-            throw new \Exception("Model with alias '{$alias}' не найдена");
+            throw new \Exception("Модель с алиасом '{$alias}' не найдена");
         }
 
         $fields = [];
@@ -47,35 +53,14 @@ class ModelMetaService
         ];
     }
 
-    public function getFieldMetadata(string $alias, string $fieldName): ?array
-    {
-        $fieldConfig = $this->registryService->getFieldConfig($alias, $fieldName);
-
-        if (!$fieldConfig) {
-            return null;
-        }
-
-        return [
-            'title' => $this->generateFieldTitle($fieldName),
-            'field_name' => $fieldName,
-            'type' => $fieldConfig['type'],
-            'default_value' => $fieldConfig['default_value'],
-            'validations' => [
-                'is_required' => $fieldConfig['validations']['is_required']
-            ],
-            'is_selected_dict' => $fieldConfig['is_selected_dict'],
-            'selected_dict_id' => $fieldConfig['selected_dict_id'],
-            'is_editable' => $fieldConfig['is_editable'] ?? ($fieldName !== 'id'),
-            'is_sortable' => $fieldConfig['is_sortable'],
-            'is_filtered' => $fieldConfig['is_filtered'],
-            'is_FK' => $fieldConfig['is_FK'],
-            'FK' => $fieldConfig['FK']
-        ];
-    }
-
+    /**
+     * Генерация заголовков для полей
+     * @param string $fieldName имя поля
+     * @return string
+     */
     private function generateFieldTitle(string $fieldName): string
     {
-        // Преобразуем snake_case в читаемый текст
+        // Преобразуем snake_case для заголовка
         $words = explode('_', $fieldName);
         $words = array_map('ucfirst', $words);
 
@@ -84,7 +69,6 @@ class ModelMetaService
             'id' => 'Идентификатор',
             'created_at' => 'Дата создания',
             'updated_at' => 'Дата обновления',
-            'deleted_at' => 'Дата удаления',
         ];
 
         if (isset($specialCases[$fieldName])) {
