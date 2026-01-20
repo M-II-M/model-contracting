d518f72796cbce4c6803ef74125074792af768b3
Mihal Ivanov <60715096+M-II-M@users.noreply.github.com>
2026-01-20 17:28:14 +0300

    - Убрал методы которые больше не используются - Добавит доки к методам

--- src/Services/ModelRegistryService.php
+++ src/Services/ModelRegistryService.php
@@ -6,6 +6,11 @@ class ModelRegistryService
 {
     private array $registeredResources = [];
 
+    /**
+     * Регистрация ресурса в реестре
+     * @param string $resourceClass класс ресурса
+     * @return void
+     */
     public function register(string $resourceClass): void
     {
         $modelClass = $resourceClass::getModelClass();
@@ -19,49 +24,31 @@ class ModelRegistryService
         ];
     }
 
+    /**
+     * Получение конфигурации модели по алиасу
+     * @param string $alias алиас модели
+     * @return array|null
+     */
     public function getResourceByAlias(string $alias): ?array
     {
         return $this->registeredResources[$alias] ?? null;
     }
 
+    /**
+     * Получение класса модели по алиасу
+     * @param string $alias алиас модели
+     * @return string|null
+     */
     public function getModelClassByAlias(string $alias): ?string
     {
         return $this->registeredResources[$alias]['model_class'] ?? null;
     }
 
-    public function getResourceClassByAlias(string $alias): ?string
-    {
-        return $this->registeredResources[$alias]['resource_class'] ?? null;
-    }
-
-    public function getAllRegisteredModels(): array
-    {
-        return array_keys($this->registeredResources);
-    }
-
-    public function getFieldConfig(string $alias, string $fieldName): ?array
-    {
-        $resource = $this->getResourceByAlias($alias);
-        return $resource['fields'][$fieldName] ?? null;
-    }
-
-    public function updateFieldConfig(string $alias, string $fieldName, array $config): bool
-    {
-        $resourceClass = $this->getResourceClassByAlias($alias);
-
-        if (!$resourceClass || !method_exists($resourceClass, 'updateField')) {
-            return false;
-        }
-
-        $resourceClass::updateField($fieldName, $config);
-        $this->registeredResources[$alias]['fields'][$fieldName] = array_merge(
-            $this->registeredResources[$alias]['fields'][$fieldName] ?? [],
-            $config
-        );
-
-        return true;
-    }
-
+    /**
+     * Получегие полей модели,по которым можно сортировать
+     * @param string $alias алиас модели
+     * @return array
+     */
     public function getSortableFields(string $alias): array
     {
         $resource = $this->getResourceByAlias($alias);
@@ -70,27 +57,16 @@ class ModelRegistryService
         return array_filter($resource['fields'], fn($field) => $field['is_sortable'] ?? true);
     }
 
+    /**
+     * Получение полей модели, по которым можно фильтровать
+     * @param string $alias алиас модели
+     * @return array
+     */
     public function getFilterableFields(string $alias): array
     {
         $resource = $this->getResourceByAlias($alias);
         if (!$resource) return [];
 
         return array_filter($resource['fields'], fn($field) => $field['is_filtered'] ?? true);
-    }
-
-    public function getEditableFields(string $alias): array
-    {
-        $resource = $this->getResourceByAlias($alias);
-        if (!$resource) return [];
-
-        return array_filter($resource['fields'], fn($field) => $field['is_editable'] ?? ($field['name'] !== 'id'));
-    }
-
-    public function getRequiredFields(string $alias): array
-    {
-        $resource = $this->getResourceByAlias($alias);
-        if (!$resource) return [];
-
-        return array_filter($resource['fields'], fn($field) => $field['validations']['is_required'] ?? false);
     }
 }
