23189d5cea086706e4a234b3e382e9c5fe961b2f
Mihal Ivanov <60715096+M-II-M@users.noreply.github.com>
2026-01-22 10:25:40 +0300

    Убрал префикс api согласно тз

--- routes/api.php
+++ routes/api.php
@@ -3,21 +3,17 @@
 use Illuminate\Support\Facades\Route;
 use MIIM\ModelContracting\Http\Controllers\ModelContractingController;
 
-Route::prefix('api')
-    ->group(function () {
+Route::get('/{alias}/meta', [ModelContractingController::class, 'getMeta'])
+    ->name('model-contracting.meta');
 
-        Route::get('/{alias}/meta', [ModelContractingController::class, 'getMeta'])
-            ->name('model-contracting.meta');
+Route::get('/{alias}', [ModelContractingController::class, 'index'])
+    ->name('model-contracting.index');
 
-        Route::get('/{alias}', [ModelContractingController::class, 'index'])
-            ->name('model-contracting.index');
+Route::post('/{alias}', [ModelContractingController::class, 'store'])
+    ->name('model-contracting.create');
 
-        Route::post('/{alias}', [ModelContractingController::class, 'store'])
-            ->name('model-contracting.create');
+Route::patch('/{alias}', [ModelContractingController::class, 'update'])
+    ->name('model-contracting.update');
 
-        Route::patch('/{alias}', [ModelContractingController::class, 'update'])
-            ->name('model-contracting.update');
-
-        Route::delete('/{alias}', [ModelContractingController::class, 'destroy'])
-            ->name('model-contracting.delete');
-    });
+Route::delete('/{alias}', [ModelContractingController::class, 'destroy'])
+    ->name('model-contracting.delete');
